2588799ac7730dd47f521fb12259ecc9103f1f1e
Willem Victor <wimpievictor@gmail.com>
2021-03-08 22:28:54 +0200

    Added parse() function

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,21 @@ mod token;
 pub use lex::*;
 pub use parsing::*;
 pub use token::*;
+
+/// Parse the given string as Wren source.
+/// 
+/// ```
+/// use rust_wren_syn::{parse, DefStmt};
+/// 
+/// const SOURCE: &str = r#"class Foo is Bar {}"#;
+/// 
+/// let statement = parse::<DefStmt>(SOURCE).expect("Parsing failed");
+/// ```
+pub fn parse<T>(source: &str) -> ParseResult<T>
+where
+    T: Parse,
+{
+    let lexer = Lexer::new(source);
+    let mut tokens = TokenStream::new(lexer);
+    T::parse(&mut tokens)
+}
